0a1c538cd211567c08f8bc166b71a191862830df
hiroto-ugajin <hrtugauga0324@gmail.com>
2023-05-04 07:24:22 +0900

    alert dialogの追加

--- app/src/main/java/jp/techacademy/hiroto/ugajin/ui/MainActivity.kt
+++ app/src/main/java/jp/techacademy/hiroto/ugajin/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log // 追加
 import android.view.View // 追加
+import androidx.appcompat.app.AlertDialog
 import jp.techacademy.hiroto.ugajin.ui.databinding.ActivityMainBinding // 追加
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -12,21 +13,44 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater) // 追加
-        val view = binding.root // 追加
-//        setContentView(R.layout.activity_main)
-        setContentView(view) // 変更
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        // View BindingでUI部品を指定
-        binding.button1.text = "ボタン"
-//        binding.button1.setOnClickListener(this)  // 追加
-
-        binding.button1.setOnClickListener {
-            Log.d("UI_PARTS", "ボタンをタップしました")
-        }
+        binding.button1.setOnClickListener(this)
+        binding.button2.setOnClickListener(this)
     }
-    // 以下の関数を追加
-    override fun onClick(v: View) {
-        Log.d("UI_PARTS", "ボタンをタップしました")
+override fun onClick(v: View) {
+    if (v.id == R.id.button1) {
+        binding.textView.text = binding.editText.text.toString()
+    } else if (v.id == R.id.button2) {
+        showAlertDialog()
+    }
+}
+    private fun showAlertDialog() {
+        // AlertDialog.Builderクラスを使ってAlertDialogの準備をする
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setTitle("タイトル")
+        alertDialogBuilder.setMessage("メッセージ")
+
+        // 肯定ボタンに表示される文字列、押したときのリスナーを設定する
+        alertDialogBuilder.setPositiveButton("肯定"){dialog, which ->
+            Log.d("UI_PARTS", "肯定ボタン")
+        }
+
+        // 中立ボタンに表示される文字列、押したときのリスナーを設定する
+        // 使わない引数の場合は「_」と記述するのがkotlinの慣習
+        alertDialogBuilder.setNeutralButton("中立"){_,_ ->
+            Log.d("UI_PARTS", "中立ボタン")
+        }
+
+        // 否定ボタンに表示される文字列、押したときのリスナーを設定する
+        alertDialogBuilder.setNegativeButton("否定"){_,_ ->
+            Log.d("UI_PARTS", "否定ボタン")
+        }
+
+        // AlertDialogを作成して表示する
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
     }
 }
